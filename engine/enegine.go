f84997684cc7ca57d2395aaa4b170497d706d238
garebare <garedaiki@gmail.com>
2021-05-05 19:33:30 +0000

    wip

--- engine/enegine.go
+++ engine/enegine.go
@@ -4,6 +4,19 @@ type Engine struct {
 	routes Route
 }
 
+type ContextFunc func(*Context)
+
 func (e Engine) New() {
+
+}
+
+func (e Engine) Get(path string, header ContextFunc) {
+	if header == nil {
+		panic("http: nil header")
+	}
+
+}
+
+func (e Engine) Post(path string, c ContextFunc) {
 
 }
