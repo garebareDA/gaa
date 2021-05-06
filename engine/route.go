4ddf08202e9692088c533453544eae36f7b97b04
garebare <garedaiki@gmail.com>
2021-05-06 20:44:51 +0000

    wip

--- engine/route.go
+++ engine/route.go
@@ -1,8 +1,34 @@
 package engine
 
-type Route struct {
-	path string
+import "net/url"
+
+type Handle func(*Context, url.Values)
+
+type node struct {
+	children     []*node
+	component    string
+	isNamedParam bool
+	methods      map[string]Handle
 }
 
-func NewRouter() {
+type Router struct {
+	tree        *node
+	rootHandler Handle
+}
+
+func New(rootHandler Handle) *Router {
+	node := node{
+		component:    "/",
+		isNamedParam: false,
+		methods:      make(map[string]Handle),
+	}
+	return &Router{tree: &node, rootHandler: rootHandler}
+}
+
+func (r *Router) Handle(method string, path string, handler Handle) {
+	if path[0] != '/' {
+		panic("Path has to start with a /.")
+	}
+
+	r.tree.addNode(method, path, handler)
 }
