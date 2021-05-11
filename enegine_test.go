7bb990d759ef1c24ac2c3cef65a142cd4678970e
garebare <garedaiki@gmail.com>
2021-05-11 11:21:03 +0000

    add:post test

--- enegine_test.go
+++ enegine_test.go
@@ -1,14 +1,17 @@
 package gaa
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestServer(t *testing.T) {
+func TestGetServer(t *testing.T) {
 	router := RouterNew(Root)
 	m := MiddleNew()
 	router.Handle("GET", "/", m.Then(func(w http.ResponseWriter, r *http.Request, u url.Values) {
@@ -16,17 +19,25 @@ func TestServer(t *testing.T) {
 	}))
 
 	w := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal("req faital")
-	}
+	req, _ := http.NewRequest("GET", "/", nil)
 	router.ServeHTTP(w, req)
 
-	if 200 != w.Code {
-		t.Fatalf("status code %d", w.Code)
-	}
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, w.Body.String(), "hello")
+}
 
-	if w.Body.String() != "hello" {
-		t.Fatalf("body %s faital", w.Body.String())
-	}
+func TestPostServer(t *testing.T) {
+	router := RouterNew(Root)
+	m := MiddleNew()
+	router.Handle("POST", "/ps", m.Then(func(w http.ResponseWriter, r *http.Request, u url.Values) {
+		fmt.Fprint(w, r.Body)
+	}))
+
+	w := httptest.NewRecorder()
+
+	body := bytes.NewBufferString("{\"msg\":{\"name\":\"foo\"}}")
+	req, _ := http.NewRequest("POST", "/ps", body)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, w.Body.String(), "{{\"msg\":{\"name\":\"foo\"}}}")
+	assert.Equal(t, w.Code, 200)
 }
